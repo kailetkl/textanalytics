b1e0d3ff434e444204d2930e636a3f4d454aa3e5
kailetkl <99379474+kailetkl@users.noreply.github.com>
2022-03-10 11:18:20 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+get_ipython().system('pip install textblob')
+
+
+# In[6]:
+
+
+from flask import Flask, request, render_template
+
+
+# In[7]:
+
+
+app = Flask(__name__)
+
+
+# In[8]:
+
+
+from textblob import TextBlob
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        t = request.form.get("text")
+        print(t)
+        r = TextBlob(t).sentiment
+        return(render_template("index.html", results=r))
+    else:
+        return(render_template("index.html", results="2"))
+
+
+# In[ ]:
+
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
